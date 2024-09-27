4bcfcbe28d4381ad9fc06ce1dc091b039f659995
schelk <isaacfschelk@outlook.com>
2024-09-27 12:44:07 -0300

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
-import { useState } from "react";
+//REACT
+import React from "react";
+import { useCallback, useEffect, useState } from "react";
+
+//CSS
 import "./App.css";
+import StartScreen from "./components/StartScreen.jsx";
+
+//DATA
+import { wordsList } from "./data/words.jsx";
 
 function App() {
+  //Stages -> Seta os estágios da página
+  const stages = [
+    { id: 1, name: "start" },
+    { id: 2, name: "game" },
+    { id: 3, name: "end" },
+  ];
+
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList);
+
   return (
-    <div>
-      <div></div>
+    <div className="App">
+      {gameStage === "start" && <StartScreen />}
+      {gameStage === "game" && <Game />}
+      {gameStage === "game" && <End />}
     </div>
   );
 }
---
+++ src/components/StartScreen.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./StartScreen.css";
+
+const StartScreen = () => {
+  return (
+    <div className="start">
+      <h1>Secret Word</h1>
+      <p>Clique no botão para iniciar o jogo</p>
+      <button>jogar</button>
+    </div>
+  );
+};
+
+export default StartScreen;
